0e15b32b841e5fb6f8277a0c3cb98a8787bb325f
krishvachhani <155625378+krishvachhani@users.noreply.github.com>
2024-08-17 19:28:33 +0530

    initial user-app completed

--- apps/user-app/app/(dashboard)/layout.tsx
+++ apps/user-app/app/(dashboard)/layout.tsx
@@ -1,34 +1,34 @@
-import { getServerSession } from "next-auth";
 import { SidebarItem } from "../../components/SidebarItem";
-import { authOptions } from "../lib/auth";
 
-export default async function Layout({
+export default function Layout({
   children,
 }: {
   children: React.ReactNode;
-}): Promise<JSX.Element> {
-  const session = await getServerSession(authOptions);
+}): JSX.Element {
   return (
-    session?.user && (
-      <div className="flex">
-        <div className="w-72 border-r border-slate-300 min-h-screen mr-4 pt-28">
-          <div>
-            <SidebarItem href={"/dashboard"} icon={<HomeIcon />} title="Home" />
-            <SidebarItem
-              href={"/transfer"}
-              icon={<TransferIcon />}
-              title="Transfer"
-            />
-            <SidebarItem
-              href={"/transactions"}
-              icon={<TransactionsIcon />}
-              title="Transactions"
-            />
-          </div>
+    <div className="flex">
+      <div className="w-72 border-r border-slate-300 min-h-screen mr-4 pt-28">
+        <div>
+          <SidebarItem href={"/dashboard"} icon={<HomeIcon />} title="Home" />
+          <SidebarItem
+            href={"/transfer"}
+            icon={<TransferIcon />}
+            title="Transfer"
+          />
+          <SidebarItem
+            href={"/transactions"}
+            icon={<TransactionsIcon />}
+            title="Transactions"
+          />
+          <SidebarItem
+            href={"/p2p"}
+            icon={<P2PTransferIcon />}
+            title="P2P Transfer"
+          />
         </div>
-        {children}
       </div>
-    )
+      {children}
+    </div>
   );
 }
 
@@ -83,6 +83,25 @@ function TransactionsIcon() {
         stroke-linecap="round"
         stroke-linejoin="round"
         d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+      />
+    </svg>
+  );
+}
+
+function P2PTransferIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
       />
     </svg>
   );
