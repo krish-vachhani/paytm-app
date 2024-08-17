ee176d80113e60ef2f351eb430dba776d590fbae
krishvachhani <155625378+krishvachhani@users.noreply.github.com>
2024-08-17 11:39:14 +0530

    created transfer page

--- apps/user-app/app/(dashboard)/layout.tsx
+++ apps/user-app/app/(dashboard)/layout.tsx
@@ -1,29 +1,34 @@
+import { getServerSession } from "next-auth";
 import { SidebarItem } from "../../components/SidebarItem";
+import { authOptions } from "../lib/auth";
 
-export default function Layout({
+export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
-}): JSX.Element {
+}): Promise<JSX.Element> {
+  const session = await getServerSession(authOptions);
   return (
-    <div className="flex">
-      <div className="w-72 border-r border-slate-300 min-h-screen mr-4 pt-28">
-        <div>
-          <SidebarItem href={"/dashboard"} icon={<HomeIcon />} title="Home" />
-          <SidebarItem
-            href={"/transfer"}
-            icon={<TransferIcon />}
-            title="Transfer"
-          />
-          <SidebarItem
-            href={"/transactions"}
-            icon={<TransactionsIcon />}
-            title="Transactions"
-          />
+    session?.user && (
+      <div className="flex">
+        <div className="w-72 border-r border-slate-300 min-h-screen mr-4 pt-28">
+          <div>
+            <SidebarItem href={"/dashboard"} icon={<HomeIcon />} title="Home" />
+            <SidebarItem
+              href={"/transfer"}
+              icon={<TransferIcon />}
+              title="Transfer"
+            />
+            <SidebarItem
+              href={"/transactions"}
+              icon={<TransactionsIcon />}
+              title="Transactions"
+            />
+          </div>
         </div>
+        {children}
       </div>
-      {children}
-    </div>
+    )
   );
 }
 
